53d280ed539d93044e30c64ad1d10f04dd3ada8c
anshul pareek <kaushik.nishant74@gmail.com>
2023-09-27 10:28:30 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { View1Component } from './Components/view1/view1.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',redirectTo:'schedular',pathMatch:'full'},
+{path:'schedular',component:View1Component}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BryntumSchedulerModule } from '@bryntum/scheduler-angular';
+ 
+import { View1Component } from './Components/view1/view1.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    View1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BryntumSchedulerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
